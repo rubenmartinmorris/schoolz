f25b702711c4e1a6b00a5d7ecad72473a9d3a6ee
rubenmartinmorris <rubenmartinmorris@gmail.com>
2021-11-05 11:53:37 +0000

    gitignore added

--- __test__/person.test.js
+++ __test__/person.test.js
@@ -1,1 +1,8 @@
-const person = require("../stuff/person");
+const Person = require("../stuff/person");
+
+describe("Person", () => {
+  test("should have a person class", () => {
+    const KKE = Person();
+    expect(KKE).toBeInstanceOf(Person);
+  });
+});
